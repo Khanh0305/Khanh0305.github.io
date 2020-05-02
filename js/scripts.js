ffab45f11a3b379ed934624387b96ec81eafcb19
Khanh0305.github.io <boychienga11@gmail.com>
2020-05-03 01:35:43 +0700

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+﻿ $(function(){
+ 	function hasClassName(inElement, inClassName)
+    {
+        var regExp = new RegExp('(?:^|\\s+)' + inClassName + '(?:\\s+|$)');
+        return regExp.test(inElement.className);
+    }
+ 
+
+    $(".classname").click(function(event) {
+    	/* Act on the event */
+    	$("#shape").toggleClass('ring');
+    	$("#shape").toggleClass('cube');
+
+    	var stage = document.getElementById('stage');
+      if (hasClassName(shape, 'ring'))
+        stage.style.webkitTransform = 'translateZ(-200px)';
+      else
+        stage.style.webkitTransform = '';
+    });
+    // JS cho phần audio
+    
+    
+
+})  
+ 
